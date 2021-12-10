d5c36710f0e613a7b55ec2379223b513f2617918
kocsol <89505081+kocsol@users.noreply.github.com>
2021-12-10 20:15:36 +0900

    12/8
    12 8 visual studio 객체화 연습

---
+++ reality/reality/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reality
+{
+    class Program
+    {
+        static void 전투(string[] args)
+        {
+        }
+    }
+
+
+    class 검사 # 검사에 대한 설계도
+    {
+        float HP = 100f;
+        int ATK = 80;
+
+        public void 내려찍기(Monster monster
+        {
+            monster.HP = monster.HP-ATK
+        }
+
+    }
+
+    class 캐릭터생성
+    {
+        검사 검사1 = new 검사();
+        검사1.내려찍기();
+
+    }
+
+    class Monster
+    {
+        public float HP = 1000f;
+
+    }
+
+
+
+
+
+
+
+
+}
