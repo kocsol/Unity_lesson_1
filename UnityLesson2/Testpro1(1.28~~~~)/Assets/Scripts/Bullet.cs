c03bfa0045631514620cbfc05f37a6cc6134e115
kocsol <89505081+kocsol@users.noreply.github.com>
2022-01-28 22:00:00 +0900

    1.28 유니티수업
    TestPro1로 옮겨감
    --적죽으면 리스폰 시키기
    저번 수업과 이번 수업 비교하기
    최적화를 위환 불렛의 최적화
    적군한테 총알이 맞으면 총알 사라지게 하기
    trycatch
    점프를 땅에 닿아쓸때만

---
+++ UnityLesson2/Testpro1(1.28~~~~)/Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody rb;
+    float interval;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //이 스크립트가 들어가 있는 게임오브젝트에게 물리적인 힘을 앞 방향으로 주어라.
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(this.gameObject.transform.forward * 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        interval += Time.deltaTime;
+        if (interval > 5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ UnityLesson2/Testpro1(1.28~~~~)/Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    int health;
+    int damage;
+    public Text hpText;
+    public Image hpBar;
+    public GameObject enemy;//선언
+
+    public EnemyManager _enemyManager; // 선언
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyManager = FindObjectOfType<EnemyManager>();//사용
+        health = 100;
+        damage = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(this.gameObject);//적은 없애고
+            //새로운 적을 하나 더 만들어주세요.
+            _enemyManager.enemyNum = 0;//사용
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Bullet")
+        {            
+            health = health - damage;
+            hpText.text = "hp :" + health;
+            float myHP = (float)(0.01f * health);
+            hpBar.fillAmount = myHP;
+            Destroy(other.gameObject);
+        }
+    }
+}
