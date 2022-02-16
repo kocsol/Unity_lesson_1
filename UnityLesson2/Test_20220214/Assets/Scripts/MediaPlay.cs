d51149ce3f59a1425a70de108ab598a1944fb6f4
kocsol <89505081+kocsol@users.noreply.github.com>
2022-02-16 21:53:14 +0900

    2/16 유니티수업
    -테라인과 플레인의 차이
    
    오디오!!
    -얻을 수 있는 좋은 사이트
    seob6285.tistory.com/212   저작권 없는 노래들 bgm 써도 돈 아내도 된다!
    
    오디오 스크립트 만들기
    
    비디오!
    
    스카이 박스
    
    포인트 라이트

---
+++ UnityLesson2/Test_20220214/Assets/Scripts/MediaPlay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class MediaPlay : MonoBehaviour
+{
+    public AudioSource audio; // 선언
+    public VideoPlayer video;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+           video.Play(); // play함수는 AudioSource 내에 있는 함수 중 하나인데 오디오 클립을 재생할 수 있다.
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            video.Pause();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            video.Stop();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+                   
+        }
+
+    }
+}
